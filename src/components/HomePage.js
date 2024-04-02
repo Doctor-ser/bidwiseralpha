a786d62bcd8b4d2a816c45ffe949787ad6fac485
Alfin-Joji <alfin.joji.joseph@gmail.com>
2024-04-03 01:29:07 +0530

    Alfin (hmpg) 03/04

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -5,13 +5,14 @@ import { Link } from 'react-router-dom';
 const HomePage = ({ darkMode }) => {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
   const images = [
+    'https://cdn.mos.cms.futurecdn.net/AEYvg9hJbdXFmWL4XKPiHk.jpg',
     'https://wallpapercave.com/wp/wp9015587.jpg',
     'https://wallpapercave.com/wp/wp6827492.jpg',
     'https://dlcdnwebimgs.asus.com/files/media/2D9FA79D-3209-484C-AF03-F216728A289C/v2/images/large/1x/kv.webp',
     'https://wallpapercave.com/wp/wp8987916.jpg',
     'https://wallpapercave.com/wp/wp8257248.jpg',
     'https://wallpapercave.com/uwp/uwp3554900.jpeg',
-    'https://wallpapercave.com/wp/wp12023119.jpg',
+    'https://wallpapercave.com/wp/wp6967920.jpg',
     'https://wallpapercave.com/wp/wp12468061.jpg',
     'https://wallpapercave.com/wp/wp5332711.jpg',
     'https://wallpapercave.com/wp/wp13479603.jpg',
@@ -45,11 +46,9 @@ const HomePage = ({ darkMode }) => {
       <div className='main'>
         <div className='row'>
           <div className='col-md-6'>
-            <h1 className="display-4">Welcome to bidwiser</h1>
+            <h1 className="display-4">Revolutionizing Online Auction</h1>
             <p className="lead">
-              Explore a seamless online auction experience with bidwiser. List your items
-              with detailed descriptions and starting bids. Bid on exciting items and
-              enjoy features like automated bidding and auction ending times.
+            Embark on a revolutionary auctioning journey with bidwiser's premier platform, boasting live global chat for dynamic interactions, intuitive bid addition for seamless engagement, and a comprehensive feedback system, all meticulously designed to set a new standard for online auctions.
             </p>
           </div>
           <div className='col-md-6'>
