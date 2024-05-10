bb7509c197a8b365d95ba36d61e94fe4c65c303c
Alfin-Joji <108486685+Alfin-Joji@users.noreply.github.com>
2024-05-10 22:02:43 +0530

    Update productdetails.js

--- src/components/productdetails.js
+++ src/components/productdetails.js
@@ -1,68 +1,96 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import {Buffer} from 'buffer';
 import { useAuth } from './AuthContext';
-import { Link,useNavigate, useParams } from 'react-router-dom';
+import { Link, useNavigate, useParams,useLocation } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import SuggestedPost from './SuggestedPost';
+import './ProductDetails.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons'; // Importing the user-circle icon
+
 
 const ProductDetails = () => {
   const [product, setProduct] = useState(null);
+  const [suggestedPosts, setSuggestedPosts] = useState([]);
   const [remainingTime, setRemainingTime] = useState(null);
   const [bidAmount, setBidAmount] = useState(0);
   const [showBidModal, setShowBidModal] = useState(false);
   const [winnerMessage, setWinnerMessage] = useState('');
+  const location = useLocation()
+  const [productImage, setProductImage] = useState(null);
+  const [imageStream,setImageStream] =useState(null)
   const auth = useAuth();
   const navigate = useNavigate();
   const { productId } = useParams(); // Get productId from URL params
-
-
 
   useEffect(() => {
     const fetchProductDetails = async () => {
       try {
         const response = await axios.get(`http://127.0.0.1:5500/api/products/${productId}`);
+        const imageResponse =  await fetch(`http://127.0.0.1:5500/api/images/${response.data.product.imageUrl}`);
+        // console.log(imageResponse)
+        const data = await imageResponse.json()
+        // const base64String = btoa(String.fromCharCode(...new Uint8Array(buffer.buffer)));
+        const base64String = Buffer.from(data.buffer.data).toString('base64')
+        const image = `data:${data.contentType};base64,${base64String}`;     
+        setImageStream(image)
+       
+
         setProduct(response.data.product);
-  
+        setProductImage(response.data.product.imageUrl);
         if (response.data.product.endTime) {
           const { ended, message } = calculateRemainingTime(response.data.product.endTime);
           setRemainingTime(ended ? message : `Bid ends in: ${message}`);
-  
-          // Check if the bid has ended
+
           if (ended) {
             try {
-              // Fetch winner of the product
               const winnerResponse = await axios.get(`http://127.0.0.1:5500/api/products/${productId}/winner`);
               const winner = winnerResponse.data.winner;
-  
+
               setProduct(prevProduct => ({
                 ...prevProduct,
-                currentBid: winner.bidAmount, // Update current bid with the winning bid amount
-                winnerEmail: winner.email // Update winner email with the email of the winning user
+                currentBid: winner.bidAmount,
+                winnerEmail: winner.email
               }));
-  
+
+
             } catch (error) {
               console.error('Error fetching winner:', error);
               setWinnerMessage('No winner found for this product.');
             }
+
+            clearInterval(intervalId);
           }
         }
+        try{
+        const category = response.data.product.category;
+        const productId= response.data.product._id;
+        //console.log(category)
+        const response1 = await axios.post(`http://127.0.0.1:5500/api/products/by-category`, { category ,productId});
+        setSuggestedPosts(response1.data);
+        }
+        catch(error){
+          //alert('no response');
+        }
+        
       } catch (error) {
         console.error('Error fetching product details:', error);
       }
     };
-  
+
     const fetchProducts = () => {
       fetchProductDetails();
     };
-  
-    // Fetch product details initially
+
     fetchProductDetails();
-  
-    // Refresh products every 5 seconds
-    const intervalId = setInterval(fetchProducts, 5000);
-  
-    // Clear interval on component unmount to prevent memory leaks
+
+    const intervalId = setInterval(fetchProducts, 500000);
+
     return () => clearInterval(intervalId);
   }, [productId]);
+
+ 
 
   const handleBid = async () => {
     if (!auth.loggedIn) {
@@ -71,37 +99,30 @@ const ProductDetails = () => {
       return;
     }
     if (!product || (product.endTime && new Date(product.endTime) < new Date())) {
-        alert('Bidding for this product has already ended.');
-        return;
-      }
-
+      alert('Bidding for this product has already ended.');
+      return;
+    }
+    const nextBidAmount = product.currentBid + 10;
+    setBidAmount(nextBidAmount);
     setShowBidModal(true);
   };
 
   const handleChatClick = (productId) => {
     return () => {
-      // Find the product associated with the productId
-    //   const product = products.find((p) => p._id === productId);
-  
-      // Check if the product exists and if the bidding has ended
       if (!product || (product.endTime && new Date(product.endTime) < new Date())) {
         alert('Bidding for this product has already ended.');
         return;
       }
-  
-      // Check if the user is logged in
+
       if (!auth.loggedIn) {
         alert('Please log in to start a chat.');
-        // Optionally, you can redirect to the login page
         navigate('/login');
         return;
       }
-  
-      // If the user is logged in and the bidding is active, navigate to the chat page
+
       navigate(`/chat/${productId}`);
     };
   };
-
 
   const placeBid = async () => {
     if (!auth.loggedIn) {
@@ -110,37 +131,27 @@ const ProductDetails = () => {
       return;
     }
 
-    // Your bid placement logic goes here
-    // This function will be called when the user confirms the bid
-    // Example: send bid amount to backend, update product details, etc.
-
     try {
-      const userId = auth.userId; // Assuming you have a way to get the user ID
-    //   const productId = product._id; // Assuming you have a way to get the product ID
+      const userId = auth.userId;
 
-      // Perform validation if needed
+      if (userId === product.userId) {
+        alert(`You are the seller of ${product.name}. You cannot place a bid for your products.`);
+        return;
+      }
+
       if (!userId || !productId || bidAmount <= 0) {
         alert('Invalid bid details.');
         return;
       }
 
-        // Use userId consistently
-        if(userId===product.userId){
-          alert(`You are the seller of ${product.name} you cannot place bid for your products`);
-          return; 
-        }
-
-      // Example: Send bid amount to backend
       const response = await axios.post('http://127.0.0.1:5500/api/placeBid', {
         productId,
         userId,
         bidAmount: Number(bidAmount),
+        category: product.category
       });
 
       if (response.status === 200) {
-        // Update product details after placing bid
-        // Fetch updated product details from backend and update state if necessary
-        // Example: setProduct(updatedProductData);
         alert('Bid placed successfully');
         setShowBidModal(false);
         setBidAmount(0);
@@ -153,11 +164,10 @@ const ProductDetails = () => {
     }
   };
 
-  // Function to format date in "dd/mm/yyyy" format
   const formatDate = (dateString) => {
     const date = new Date(dateString);
     const day = date.getDate().toString().padStart(2, '0');
-    const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Months are zero-based
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
     const year = date.getFullYear();
     return `${day}/${month}/${year}`;
   };
@@ -185,41 +195,76 @@ const ProductDetails = () => {
       message: `${endDateFormatted}, ${hours}h ${minutes}m ${seconds}s left`,
     };
   };
+
   return (
-    <div>
+    <div className='full'>
       {product ? (
-        <div>
-          <h2>{product.name}</h2>
-          <p>Description: {product.description}</p>
-          <Link to={`/sellerinfo/${product.userId}`} className="card-text">Seller Info</Link>
-          <p>Starting Bid: &#8377;{product.startingBid}</p>
-          <p>Current Bid: &#8377;{product.currentBid}</p>
-          <p>Product Added By: {product.userId}</p>
-          {product.winnerEmail && <p>Winner Email: {product.winnerEmail}</p>}
-          {product.winnerEmail && <p>Highest Bid: &#8377;{product.currentBid}</p>}
-          {winnerMessage && <p>{winnerMessage}</p>}
-          {product.endTime && (
-            <p>
-              {remainingTime ? remainingTime : "Bidding for this product has ended."}
-            </p>
-          )}
+        <div className='pro-details'>
+          {productImage && (
+           
+             <img
+              src={imageStream}
+              alt={product.name}
+              className="pro-img det-img-thumbnail"
+            />
           
-          {/* Conditionally render buttons if there is remaining time or no winner */}
-          {remainingTime || winnerMessage ? (
-            <div>
-              <button className="btn-p cart px-auto" onClick={handleBid}>
-                Place Bid
-              </button>
-              <button className="chat-btn" onClick={handleChatClick(productId)}>
-                Chat
-              </button>
+          
+          )}
+
+          <div className='pro-info'>
+            <div className='c'>
+            <h2 className='hn'>{product.name}</h2>
+            <div className='p-de'>
+              <p className="description">{product.description}</p>
+              <div className="sel">
+                <Link to={`/sellerinfo/${product.userId}`}>
+                  <FontAwesomeIcon icon={faUserCircle} className="us-ic" />
+                  <span className='id'>&nbsp;{product.userId}</span>
+                </Link>
+              </div>
+              <p className="bid-details cur">Current Bid:&nbsp;&#8377;<strong>{product.currentBid}</strong></p>
+              <p className="bid-details">Starting Bid: &#8377;{product.startingBid}</p>
+              {product.winnerEmail && <p className="winner-email">Winner Email: {product.winnerEmail}</p>}
+              {product.winnerEmail && <p className="highest-bid">Highest Bid: &#8377;{product.currentBid}</p>}
+              {winnerMessage && <p className="winner-message">{winnerMessage}</p>}
+              {product.endTime && (
+                <p className="end-time">
+                  {remainingTime ? remainingTime : "Bidding for this product has ended."}
+                </p>
+              )}
+
+              {/* Conditionally render buttons if there is remaining time or no winner */}
+              {remainingTime || winnerMessage ? (
+                <div className="button-container bc">
+                  <button className="bp" onClick={handleBid}>
+                    <strong>Place Bid</strong>
+                  </button>
+                  <button className="bp" onClick={handleChatClick(productId)}>
+                  <strong>Chat</strong>
+                  </button>
+                </div>
+              ) : null}
             </div>
-          ) : null}
+            </div>
+          </div>
         </div>
+        
+        
       ) : (
         <p>Loading...</p>
       )}
-  
+      
+      {/* Suggested Post */}
+      <div className='conatiner-suggestedposts'> 
+        <h3 className='sug'>Similar products</h3>
+        <div className="suggested-post">
+       {suggestedPosts.map((post) => (
+            <SuggestedPost key={post._id} post={post} />
+          ))}
+        </div>
+      </div>
+      
+
       {showBidModal && (
         <div className="modal" style={{ display: 'block' }}>
           <div className="modal-dialog">
@@ -229,17 +274,17 @@ const ProductDetails = () => {
                 <button type="button" className="btn-close" onClick={() => setShowBidModal(false)}></button>
               </div>
               <div className="modal-body">
-                <p>Current Bid: &#8377;{product.currentBid}</p>
                 <input
                   type="number"
                   placeholder="Enter your bid amount"
-                  className="form-control"
+                  className="form-control f-c"
                   value={bidAmount}
                   onChange={(e) => setBidAmount(e.target.value)}
                 />
+                <p>Current Bid: &#8377;{product.currentBid}</p>
               </div>
               <div className="modal-footer">
-                <button className="btn-primary2" onClick={placeBid}>
+                <button className="btn-primary2 b-cb" onClick={placeBid}>
                   Confirm Bid
                 </button>
               </div>
@@ -248,6 +293,7 @@ const ProductDetails = () => {
         </div>
       )}
     </div>
+   
   );
 };
 
